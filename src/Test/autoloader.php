e854b90fb72eec6e3ef85a80ed8c23fcc7b14a00
Taniko Yamamoto <kirasicecreamm@gmail.com>
2024-09-22 08:23:23 +0900

    3.2: Promise ergonomic improvements
    - Rejection verification is now done at destruction time rather than
    rejection time. This means that Promise rejection handlers can be late
    bound, fixing #1.
    
    - Promises now return their setup callbacks when the resolve or reject
    callback arguments are called. This avoids the need to manually return
    after a resolution or rejection. This change breaks backwards
    compatibility, and so is disabled by default for Deferred implementers.
    
    - To facilitate the previous thing, Promises now internally have a
    version number which controls their behavior. For anonymous Promises
    (those constructed via new Promise(callable)), the version number is
    always the latest. Deferred implementers must change the version on
    their own.

--- src/Test/autoloader.php
+++ src/Test/autoloader.php
@@ -2,8 +2,10 @@
 spl_autoload_register(function($className) {
     $className = str_replace("\\", "/", $className);
 
-    include explode("CoffeeRequest/", str_replace("\\", "/", __DIR__))[0] . 
-        str_replace("YukisCoffee", "", $className) .
+    var_dump(str_replace("\\", "/", __DIR__));
+
+    include explode("Test", str_replace("\\", "/", __DIR__))[0] . 
+        str_replace("YukisCoffee/CoffeeRequest", "", $className) .
         ".php"
     ;
 });
